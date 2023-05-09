7556b10f782ff57a2c4ce41324a4c3716a09e9e5
amr <amrkhaled122222@gmail.com>
2023-05-09 23:13:42 +0100

    api done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\api\ApiNutritionController;
 use App\Http\Controllers\api\ApiStatusController;
+use App\Http\Controllers\api\ApiWorkoutController;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TestApiController;
 use Illuminate\Http\Request;
@@ -22,23 +23,32 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::group([
-//    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
+//Route::group([
+////    'middleware' => 'api',
+//    'prefix' => 'auth'
+//], function ($router) {
+//
+//
+//});
 
 
-});
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
 //Route::get('/users',[TestApiController::class,'index']);
+
+
+
+
+
 Route::group([
     'middleware' => 'jwt.verify',
 ], function ($router) {
-
-    Route::post('/logout', [AuthController::class, 'logout']);
 //    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
     Route::get('/show-nutrition-system',[ApiNutritionController::class,'apiBuildNitritionSystem']);
+    Route::get('/show-workout-system',[ApiWorkoutController::class,'apiBuildWorkoutSystem']);
     Route::get('/show-status',[ApiStatusController::class,'apiShowStatus']);
 });
