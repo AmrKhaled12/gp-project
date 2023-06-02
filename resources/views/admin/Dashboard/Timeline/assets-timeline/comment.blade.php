bc384c8fd6c9fdd5e4b783ebbb331343bab260e8
OmarHesham <omarhesham11a@gmail.com>
2023-06-02 05:42:10 +0300

    livewire-2

--- resources/views/admin/Dashboard/Timeline/assets-timeline/comment.blade.php
+++ resources/views/admin/Dashboard/Timeline/assets-timeline/comment.blade.php
@@ -5,30 +5,6 @@
             <li>
                 <p class="user__name">Amr Khaled</p> gamd
             </li>
-            <li>
-                <p class="user__name">Fouad Mahmoud</p> you doing well
-            </li>
-            <li>
-                <p class="user__name">Omar Hesham</p> nice
-            </li>
-            <li>
-                <p class="user__name">Amr Khaled</p> gamd
-            </li>
-            <li>
-                <p class="user__name">Fouad Mahmoud</p> you doing well
-            </li>
-            <li>
-                <p class="user__name">Omar Hesham</p> nice
-            </li>
-            <li>
-                <p class="user__name">Amr Khaled</p> gamd
-            </li>
-            <li>
-                <p class="user__name">Fouad Mahmoud</p> you doing well
-            </li>
-            <li>
-                <p class="user__name">Omar Hesham</p> nice
-            </li>
         </ul>
     </div>
 </div>
